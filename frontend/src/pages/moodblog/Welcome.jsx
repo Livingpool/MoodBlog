1acbf670b82edd54cd559983b5c5902fe7b1c720
only18days <only18days5566@gmail.com>
2023-10-24 15:46:05 +0800

    Edit welcome page

--- frontend/src/pages/moodblog/Welcome.jsx
+++ frontend/src/pages/moodblog/Welcome.jsx
@@ -75,14 +75,14 @@ export default function MoodBlogComponent() {
                                 color="white"
                                 className="mb-4 text-3xl md:text-4xl lg:text-5xl"
                             >
-                                MoodWave 心情日記
+                                你知道嗎？
                             </Typography>
                             <Typography
                                 variant="lead"
                                 color="white"
                                 className="mb-12 opacity-80"
                             >
-                                哈囉，寫日記的超級明星🌟！<br></br>你知道嗎，你的每一句話都是一顆閃亮的星星✨，<br></br>而我正迫不及待地想要看到它們閃爍！<br></br>無論今天過得怎樣，都值得被記錄。
+                                日記一詞源自拉丁語 "diarium"，意思是 "每日"。<br></br>最早的日記據說可以追溯到西元2世紀，<br></br>由羅馬皇帝馬庫斯·奧勒留所寫，這本日記稱為《沉思錄》。
                             </Typography>
                             {/* <div className="flex justify-center gap-2">
                                 <Button size="lg" color="white">
@@ -108,14 +108,14 @@ export default function MoodBlogComponent() {
                                 color="white"
                                 className="mb-4 text-3xl md:text-4xl lg:text-5xl"
                             >
-                                MoodWave 心情日記
+                                為什麼日記是最懶惰的書🌟
                             </Typography>
                             <Typography
                                 variant="lead"
                                 color="white"
                                 className="mb-12 opacity-80"
                             >
-                                哈囉，寫日記的超級明星🌟！<br></br>你知道嗎，你的每一句話都是一顆閃亮的星星✨，<br></br>而我正迫不及待地想要看到它們閃爍！<br></br>無論今天過得怎樣，都值得被記錄。
+                                <br></br>因為它們只有一頁，而且一天只寫一次！
                             </Typography>
                             {/* <div className="flex justify-center gap-2">
                                 <Button size="lg" color="white">
